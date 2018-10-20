28a90ca54edd7cba749357eadeac696a03d24695
swissChili <sasha@fastmail.com>
2018-10-20 16:27:14 -0700

    Added basic requests

--- backend/db.py
+++ backend/db.py
@@ -32,9 +32,28 @@ print(json.dumps(structure))
 client = MongoClient()
 db = client.rootedvocab
 words = db.words
-
+roots = db.roots
 
 # call this for different JSON files, like for the roots and words
 def load_local(json_file):
-    for i in json.loads(json_file):
+    with open(json_file, "r") as data:
+        f = data.read()
+        data.close()
+
+    print(f)
+    for i in json.loads(f):
         yield i
+
+# load the roots to the DB
+for root in load_local("roots.json"):
+    roots.insert_one(root)
+
+for word in load_local("words.json"):
+    words.insert_one(word)
+
+def get_word_definition(word):
+    return words.find_one({"word": word})["definition"]
+
+def get_word_roots(word):
+    for root in words.find_one({"word": word})["roots"]:
+        yield root
--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,24 @@
 from bottle import route, run, get
 import json
 import db
+import os
 
 @route("/")
 def return_all():
     return db.dump()
+
+@route("/words/<words>")
+def return_json(words):
+    words = words.split("-")
+    rwords = {}
+    rroots = {}
+    for word in words:
+        print(word)
+        rwords[word] = {
+                "roots": [ w for w in db.get_word_roots(word) ],
+                "definition": db.get_word_definition(word)
+            }
+        print(rwords[word])
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))

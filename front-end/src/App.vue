6a2c3da103cf382dbca3d15b8d0cc005305ca62f
Ronan <rsf.sho@gmail.com>
2018-10-20 14:54:08 -0700

    added mini reset.css

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <router-view/>
   </div>
+
 </template>
 
-<style lang="scss">
-</style>
+<script>
+  import 'minireset.css'
+  export default {
+    name: 'appContainer'
+  }
+</script>

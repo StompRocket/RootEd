3b457340bed02b7fab4745e01fe732f1b203bb6c
swissChili <sasha@fastmail.com>
2018-10-20 19:25:08 -0700

    cors

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: cd backend && pipenv install
-web: cd backend && pipenv run python3 server.py
--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,21 @@ import json
 import db
 import os
 
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
 @route("/")
+@enable_cors
 def return_all():
     return db.dump()
 
@@ -24,14 +38,15 @@ def data_from_words(words):
         "roots": rroots
     }
 @route("/words/<words>")
+@enable_cors
 def return_json(words):
     words = words.split("-")
     return data_from_words(words)
-
+@enable_cors
 @route("/sets")
 def get_set():
     return json.dumps(db.get_sets())
-
+@enable_cors
 @route("/set/<set_id>")
 def set_data(set_id):
     return json.dumps(data_from_words(db.get_study_set(set_id)))

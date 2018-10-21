5507bb4e837d8ea9faf1b463f28f4cb93d23431c
Ronan <rsf.sho@gmail.com>
2018-10-21 01:16:22 -0700

    test filew

--- front-end/src/views/Studying.vue
+++ front-end/src/views/Studying.vue
@@ -22,11 +22,56 @@
     name: "studying",
     data() {
       return {
-
+        set: {},
+        combined: [],
+        current: {}
       }
     },
     created() {
+      this.$parent.loading = true
+      fetch(this.$parent.baseURL + '/set/' + this.$route.params.id).then(response => response.json()).then(data => {
+        this.set = data;
+        this.$parent.loading = false
+        this.compute()
+        this.getWeightedQuestion()
+      })
+    },
+    methods: {
+      getWeightedQuestion() {
+        let weighted = []
+        for (let index in this.combined) {
+          let item = this.combined[index]
+          console.log(item)
+          for (let i = 0; i < item.weight; i++) {
+            weighted.push(item)
+          }
 
+        }
+        console.log(weighted)
+
+      },
+      compute() {
+        for (let root in this.set.roots) {
+          let def = this.set.roots[root]
+          let rootData = {
+            root: root,
+            def: def,
+            weight: 5
+          }
+          this.combined.push(rootData)
+        }
+        for (let word in this.set.words) {
+          let def = this.set.words[word].definition
+          let roots = this.set.words[word].roots
+          let wordData = {
+            word: word,
+            roots: roots,
+            def: def,
+            weight: 0
+          }
+          this.combined.push(wordData)
+        }
+      }
     }
   }
 </script>
--- front-end/src/views/Test.vue
+++ front-end/src/views/Test.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="page study">
+
+    </div>
 </template>
 
 <script>
@@ -11,36 +13,7 @@ export default {
     };
   },
   created() {
-    setTimeout(() => {
-      let someting = {
-        words: {
-          biology: {
-            roots: ["bio", "logy"],
-            definition:
-              "The science of life and of living organisms, including their structure, function, growth, origin, evolution, and distribution. It includes botany and zoology and all their subdivisions."
-          },
-          biochemistry: {
-            roots: ["bio", "chem"],
-            definition:
-              "The study of the chemical substances and vital processes occurring in living organisms; biological chemistry; physiological chemistry."
-          }
-        },
-        roots: {
-          bio: {
-            def: "life",
-            words: ["biology", "biochemistry"]
-          },
-          logy: {
-            def: "the study of",
-            words: ["biology", "psychology"]
-          },
-          chem: {
-            def: "small atom thing",
-            words: ["chemistry", "chemical"]
-          }
-        }
-      };
-    }, 0.5);
+
   }
 };
 </script>

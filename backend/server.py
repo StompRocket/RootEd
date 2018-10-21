2dbec34cee26b908444ba821d8a9d96f0c9beb1f
swissChili <sasha@fastmail.com>
2018-10-21 11:06:51 -0700

    changed host

--- backend/server.py
+++ backend/server.py
@@ -84,4 +84,4 @@ def get_root_words(root):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8081))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
